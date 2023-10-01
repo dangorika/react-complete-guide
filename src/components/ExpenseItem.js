d7ffaeccbfb5a5e40131f6e24f7749b366984bd0
Olha Yevtushenko <dangorika@Olhas-Laptop.local>
2023-10-01 21:42:04 +0200

    Assignment 1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car insurance';
-  const expenseAmount = 297.64;
-  
+function ExpenseItem({ title, amount, date }) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
